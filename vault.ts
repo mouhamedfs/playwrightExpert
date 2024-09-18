22d6a91570d7cea09ae8022da8b3fef8a53f2068
mouhamedfs <mouhamedfsane@gmail.com>
2024-09-18 14:51:19 +0200

    implement vault

---
+++ vault.ts
@@ -0,0 +1,20 @@
+const vault = require('node-vault')();
+
+// Load environment variables from the system (not the .env file)
+const getSecret = async (path) => {
+    try {
+        vault.address = process.env.VAULT_ADDR;
+        vault.token = process.env.VAULT_TOKEN;
+
+        if (!vault.address || !vault.token) {
+            throw new Error('VAULT_ADDR or VAULT_TOKEN not found in environment variables');
+        }
+
+        const result = await vault.read(path);
+        return result.data.data.value;  // Retrieve the secret value
+    } catch (err) {
+        console.error(`Error fetching secret: ${err.message}`);
+    }
+};
+
+module.exports = getSecret;
